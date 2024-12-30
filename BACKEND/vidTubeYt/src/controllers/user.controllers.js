c00ed0b880aad2fd8abecce8260f69174212a7d8
krg <kishanghosh090@gmail.com>
2024-12-30 17:51:48 +0530

    file added

--- BACKEND/vidTubeYt/src/controllers/user.controllers.js
+++ BACKEND/vidTubeYt/src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // generate access token and refresh token
 const generateAccessAndRefreshToken = async (userId) => {
@@ -360,6 +361,61 @@ const getUserProfile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, channel[0], "channel found successfully"));
 });
 
+// get watched videos
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    userName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfully"
+      )
+    );
+});
+
 // export controllers
 export {
   registerUser,
@@ -370,4 +426,5 @@ export {
   getCurrentUser,
   updateAccountDetails,
   getUserProfile,
+  getWatchHistory,
 };
