bf585f1186db8d2f5c644b2a69e4387282039a74
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-21 17:13:35 +0530

    added

---
+++ hiteshSirFrontend/reactYT/counter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <>
+      <h1>hello chai from chai code </h1>
+      <h2>counter value: 5</h2>
+      <button>Add Value</button>
+      <button>Remove value</button>
+    </>
+  );
+}
+
+export default App;
