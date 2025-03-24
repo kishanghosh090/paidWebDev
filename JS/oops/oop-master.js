b0f6cfa33c40a4f1327ec3b6ac92bd3700e1750b
kishanghosh090 <kishanghosh090@gmail.com>
2025-03-24 18:44:12 +0530

    getter setter encapsulation polymorphisom

--- JS/oops/oop-master.js
+++ JS/oops/oop-master.js
@@ -54,3 +54,105 @@ let myCar = new Car("Toyota", "Corolla");
 // console.log(myCar.drive(),"\n", myCar.start());
 
 // let vehOne = new Vehicle("Tata", "safari"); // not invvcoked without new keyword
+
+// Encapulation----------------------------------------
+
+class BankAccount {
+  #balance = 12;
+
+  deposite(amount) {
+    this.#balance += amount;
+    return this.#balance;
+  }
+  getBalance() {
+    return this.#balance;
+  }
+}
+
+let myAccount = new BankAccount();
+// console.log(myAccount.deposite(100));
+// console.log(myAccount.#balance)
+// console.log(myAccount.getBalance());
+
+// Abstraction----------------
+
+class CoffeMachine {
+  start() {
+    // call a db
+    // filter value
+    return `Coffe machine is started`;
+  }
+  brewCoffee() {
+    // complex calculation
+    return `Coffe is brewed`;
+  }
+  pressStartButton() {
+    let msgone = this.start();
+    let msgTwo = this.brewCoffee();
+    return `${msgone} \n ${msgTwo}`;
+  }
+}
+
+let myMachine = new CoffeMachine();
+// console.log(myMachine.start());
+// console.log(myMachine.brewCoffee());
+
+// console.log(myMachine.pressStartButton());
+
+// polymorphism ---------------------
+
+class Bird {
+  fly() {
+    return `Flying...`;
+  }
+}
+
+class Penguin extends Bird {
+  fly() {
+    return `Penguin can't fly`;
+  }
+}
+
+class Sparrow extends Bird {
+  fly() {
+    return `Sparrow is flying`;
+  }
+}
+
+class Calculator {
+  static add(a, b) {
+    return a + b;
+  }
+}
+
+let miniCalc = new Calculator();
+
+// console.log(Calculator.add(2, 3));
+// console.log(miniCalc.add(2, 3)); // not a function error will be thrown
+
+// getter and setters
+
+class Employee {
+  #salary;
+  constructor(name, salary) {
+    if (salary < 0) {
+      throw new Error("Salary can't be negative");
+    }
+    this.name = name;
+    this.#salary = salary;
+  }
+  get salary() {
+    return `Salary is ${this.#salary}`;
+  }
+  set salary(value) {
+    if (value < 0) {
+      throw new Error("Salary can't be negative");
+    }
+    this.#salary = value;
+  }
+}
+
+let empOne = new Employee("jhon", 100000);
+
+// console.log(empOne._salary);
+console.log(empOne.salary);
