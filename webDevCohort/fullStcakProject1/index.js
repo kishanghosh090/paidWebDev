55912d68fe538a8868e874260504904b172c4d60
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-17 10:50:40 +0530

    added

---
+++ webDevCohort/fullStcakProject1/index.js
@@ -0,0 +1,41 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+
+
+const app = express()
+console.log(app);
+
+
+app.use(cors({
+    origin: "http://localhost:5321",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res, next) => {
+    return res.send("hello from chai")
+    // next()
+})
+
+app.get("/piyush", (req, res) => {
+    res.send("piyush!!")
+})
+
+
+
+
+// app.use((req, res) => {
+//     res.json({ "status": 2001 })
+// })
+
+app.listen(PORT, () => {
+    console.log(`My Server is listing at http://localhost:${PORT}`);
+
+})
