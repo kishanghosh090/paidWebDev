96c75e85e381974b1a0afe30411ff47f7a27fd87
kishanghosh090 <kishanghosh090@gmail.com>
2025-04-18 21:17:46 +0530

    added

---
+++ procodderNodeJs/basicOfNodeJS/01/try/index.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <= 100; i++) {
+    console.log(i)
+}
+
+console.log("kishan rana ghosh");
+
--- procodderNodeJs/basicOfNodeJS/01/try/loop.sh
+++ procodderNodeJs/basicOfNodeJS/01/try/loop.sh
@@ -1,1 +1,4 @@
-for i in {1..10}; do touch "app$i.js"; done
+# for i in {1..10}; do touch "app$i.js"; done
+
+#for i in {1..10}; do rm "app$i.js"; done
+
