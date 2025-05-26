a14b22810f97f13168b4a69f1c8f32a85bae5395
kishanghosh090 <kishanghosh090@gmail.com>
2025-05-26 14:13:43 +0530

    added

---
+++ deltaBatch/nodejs/script.js
@@ -0,0 +1,13 @@
+console.log("hello world from chai");
+
+// console.log(process);
+
+console.log(process.argv);
+
+const { sum } = require('./math.js');
+const { sub } = require('./math.js');
+// console.log(require('./math.js'));
+
+const fun = require('./math.js');
+console.log(fun.sum(1, 2));
+console.log(fun.sub(1, 2));
