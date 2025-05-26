3689f2e369f09243a488c2e442dbfa6f6ddc23a6
kishanghosh090 <kishanghosh090@gmail.com>
2025-05-26 19:20:08 +0530

    added

---
+++ deltaBatch/nodejs/foods/akash.js
@@ -0,0 +1,7 @@
+const app = require('./apple.js');
+const ban = require('./banana.js');
+const man = require('./mango.js');
+
+let fruit = [app, ban, man];
+
+module.exports = fruit
--- deltaBatch/nodejs/script.js
+++ deltaBatch/nodejs/script.js
@@ -2,12 +2,15 @@ console.log("hello world from chai");
 
 // console.log(process);
 
-console.log(process.argv);
+// console.log(process.argv);
 
 const { sum } = require('./math.js');
 const { sub } = require('./math.js');
 // console.log(require('./math.js'));
 
 const fun = require('./math.js');
-console.log(fun.sum(1, 2));
-console.log(fun.sub(1, 2));
+// console.log(fun.sum(1, 2));
+// console.log(fun.sub(1, 2));
+
+const fruit = require('./foods/akash.js');
+console.log(fruit);
