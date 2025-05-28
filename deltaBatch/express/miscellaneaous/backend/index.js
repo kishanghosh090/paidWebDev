e481bd8d581cb01f9a6d30773294db2e314f5fa2
kishanghosh090 <kishanghosh090@gmail.com>
2025-05-28 20:10:22 +0530

    added

--- deltaBatch/express/miscellaneaous/backend/index.js
+++ deltaBatch/express/miscellaneaous/backend/index.js
@@ -1,11 +1,33 @@
 import express from "express";
+import cors from "cors"
+
 
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: "http://localhost:5500",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+})
+
+app.get("/register", (req, res) => {
+
+    res.send("Register");
+})
+
+app.post("/register", (req, res) => {
+    console.log(req.body);
+    console.log(req.headers);
+
+    res.send("Register");
 })
 
 
