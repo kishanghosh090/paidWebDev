929792feae25d65e0ec87b68b919be667f30c004
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-07-26 22:54:09 +0530

    ai video

---
+++ NEXTJS/nextchaicodevidepwithai/app/api/auth/register/route.ts
@@ -0,0 +1,30 @@
+import dbConnect from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "Name, email and password are required" },
+        { status: 400 }
+      );
+    }
+    await dbConnect();
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.create({ name, email, password });
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+    NextResponse.json({ error: error }, { status: 500 });
+  }
+}
