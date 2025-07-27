45cd6ab36656b9909fab58ade8057eb7fb5c0a65
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-07-27 22:55:14 +0530

    ai saas

--- NEXTJS/aipoweredsaas/postcss.config.mjs
+++ NEXTJS/aipoweredsaas/postcss.config.mjs
@@ -1,5 +1,17 @@
+import themes from "daisyui/theme/object";
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  themes: [
+    {
+      light: {
+        ...themes["[data-theme=light]"],
+      },
+      dark: {
+        ...themes["[data-theme=dark]"],
+      },
+    },
+  ],
 };
 
 export default config;
