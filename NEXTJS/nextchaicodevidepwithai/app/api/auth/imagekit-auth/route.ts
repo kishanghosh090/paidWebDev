4f038b39843e05e310787a6cc8e6281eb99ed7b8
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-07-27 18:02:12 +0530

    imagekit

---
+++ NEXTJS/nextchaicodevidepwithai/app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,18 @@
+import { getUploadAuthParams } from "@imagekit/next/server";
+
+export async function GET() {
+  try {
+    const authenticationParameters = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    });
+
+    return Response.json({
+      authenticationParameters,
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: error }, { status: 500 });
+  }
+}
