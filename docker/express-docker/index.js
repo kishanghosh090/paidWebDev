beda99adc3a13a39bf6d85c8957956f1887971dd
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-08-05 01:15:32 +0530

    docker

--- docker/express-docker/index.js
+++ docker/express-docker/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
-app.listen(3000, () => console.log('Server is running on http://localhost:3000'))
+app.listen(3000, () => console.log(`Server is running on http://localhost:${PORT}`))
