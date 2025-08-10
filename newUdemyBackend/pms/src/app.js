147cbc47804c1e2dea0e3fdffde2f8598dbffe36
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-08-10 17:38:46 +0530

    add ApiResponse class

--- newUdemyBackend/pms/src/app.js
+++ newUdemyBackend/pms/src/app.js
@@ -2,13 +2,15 @@ import express from "express"
 import cors from "cors"
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb", extended: true }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 
 app.use(cors({
     origin: "*",
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }))
 
 export { app }
