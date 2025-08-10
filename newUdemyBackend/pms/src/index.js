98e0625a043fee58d6f985112a3a2e5d34fd27d0
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-08-10 17:47:27 +0530

    add ApiError class

--- newUdemyBackend/pms/src/index.js
+++ newUdemyBackend/pms/src/index.js
@@ -5,6 +5,9 @@ import { app } from "./app.js";
 const PORT = process.env.PORT || 3001;
 
 
+console.log(Error.captureStackTrace());
+
+
 
 app.listen(PORT, () => {
     console.log(`server is listing at PORT : ${PORT}`);
