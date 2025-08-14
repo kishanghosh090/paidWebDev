d9431c1ad77da48d0a5bfe47e5582f26df830737
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-08-14 19:47:50 +0530

    add validators

--- newUdemyBackend/pms/src/routes/auth.routes.js
+++ newUdemyBackend/pms/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 
 import { loginUser, registerUser } from "../controllers/Auth.controller.js"
-import { validate } from "../middlewares/validator.middleare.js";
+import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js"
 
 const router = Router();
