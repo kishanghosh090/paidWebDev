ba208c89f6e142aa522df269d7b50a7b21243975
KISHAN RANA GHOSH <kishanghosh090@gmail.com>
2025-08-16 18:04:41 +0530

    add all user routes

--- newUdemyBackend/pms/src/routes/auth.routes.js
+++ newUdemyBackend/pms/src/routes/auth.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
 
-import { loginUser, logoutUser, registerUser } from "../controllers/Auth.controller.js"
+import { changePassword, forgotPasswordRequest, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, resendEmailVerification, restForgotPassword, verifyEmail } from "../controllers/Auth.controller.js"
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator, userLoginValidator } from "../validators/index.js"
+import { userRegisterValidator, userLoginValidator, userRestForgotPasswordValidator, userChangeCurrentPasswordValidator } from "../validators/index.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router();
@@ -19,9 +19,47 @@ router.route("/login").post(
     validate,
     loginUser
 )
+router.route("/verify-email/:verificationToken").get(
+    verifyEmail
+)
 
+router.route("/refresh-token").post(
+    refreshAccessToken
+)
+
+router.route("/forgotpassword").get(
+    userLoginValidator(),
+    validate,
+    forgotPasswordRequest
+)
+
+router.route("/resetPassword/:resetToken").post(
+    userRestForgotPasswordValidator(),
+    validate,
+    restForgotPassword
+)
+
+
+
+
+
+// secured routes
 router.route("/logout").post(
     verifyJWT,
     logoutUser
+)
+router.route("/currentUser").post(
+    verifyJWT,
+    getCurrentUser
+)
+router.route("/changePassword").post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changePassword
+)
+router.route("/resendEmailVerification").post(
+    verifyJWT,
+    resendEmailVerification
 )
 export default router
