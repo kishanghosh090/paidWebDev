cf81e940bdb64278f473fcf824d4f8a95b7da513
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-23 18:39:51 +0530

    node fs

---
+++ NODEJSUDEMY/modules/index.js
@@ -0,0 +1,9 @@
+// modules
+//  1 built in modules
+//  2 3rd party (external modules)
+//  3 custom modules
+
+// file system
+
+const fs = require("fs")
+console.log(fs.readFileSync("./modules/try.txt","utf-8"))
