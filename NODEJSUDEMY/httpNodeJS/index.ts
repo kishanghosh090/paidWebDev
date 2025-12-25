fe1109ddc29f95e4ad060db3e462bcdfb3074b83
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-25 16:31:07 +0530

    added node concepts

--- NODEJSUDEMY/httpNodeJS/index.ts
+++ NODEJSUDEMY/httpNodeJS/index.ts
@@ -5,8 +5,17 @@ const server = http.createServer(function (
   res: ServerResponse<IncomingMessage>
 ) {
   console.log(req.headers);
+  const path = req.url;
 
   if (req.method == "GET") {
+  }
+  if (req.method == "POST") {
+    switch (path) {
+      case "/tweet":
+        return res
+          .writeHead(200, { "content-type": "application/json" })
+          .end("your tweet is created");
+    }
   }
   res.writeHead(200, { "content-type": "application/json" });
   res.end("thans for visiting");
