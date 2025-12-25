2b51dde0a58c57e451bcb021e79961696990e053
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-25 17:12:00 +0530

    express

--- NODEJSUDEMY/httpNodeJS/index.ts
+++ NODEJSUDEMY/httpNodeJS/index.ts
@@ -1,27 +1,45 @@
-import http, { IncomingMessage, ServerResponse } from "http";
+// node ---------------------------------------------------------
+// import http, { IncomingMessage, ServerResponse } from "http";
 
-const server = http.createServer(function (
-  req: IncomingMessage,
-  res: ServerResponse<IncomingMessage>
-) {
-  console.log(req.headers);
-  const path = req.url;
+// const server = http.createServer(function (
+//   req: IncomingMessage,
+//   res: ServerResponse<IncomingMessage>
+// ) {
+//   console.log(req.headers);
+//   const path = req.url;
 
-  if (req.method == "GET") {
-  }
-  if (req.method == "POST") {
-    switch (path) {
-      case "/tweet":
-        return res
-          .writeHead(200, { "content-type": "application/json" })
-          .end("your tweet is created");
-    }
-  }
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end("thans for visiting");
-});
+//   if (req.method == "GET") {
+//   }
+//   if (req.method == "POST") {
+//     switch (path) {
+//       case "/tweet":
+//         return res
+//           .writeHead(200, { "content-type": "application/json" })
+//           .end("your tweet is created");
+//     }
+//   }
+//   res.writeHead(200, { "content-type": "application/json" });
+//   res.end("thans for visiting");
+// });
+
+// const PORT = 8012;
+// server.listen(PORT, () => {
+//   console.log(`http server is listning at PORT ${PORT}`);
+// });
+
+// express ----------------
+
+import express, { type Request, type Response } from "express";
+const app = express();
 
 const PORT = 8012;
-server.listen(PORT, () => {
-  console.log(`http server is listning at PORT ${PORT}`);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(201).json({
+    'sec-ch-ua-platform':'',
+    msg: "hello from kishna",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`server is listing at PORT ${PORT}`);
 });
---
+++ NODEJSUDEMY/httpNodeJS/readme.md
@@ -0,0 +1,3 @@
+# major.minor.patchs
+
+"typescript": "^5.9.3"
