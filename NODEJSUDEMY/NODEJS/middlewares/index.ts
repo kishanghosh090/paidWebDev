24d3356957af5d6bab399e7d6abae37a070cf531
kishanghosh090 <kishanghosh090@gamil.com>
2026-01-06 00:13:43 +0530

    add router and global middleware

--- NODEJSUDEMY/NODEJS/middlewares/index.ts
+++ NODEJSUDEMY/NODEJS/middlewares/index.ts
@@ -8,8 +8,48 @@ const PORT = process.env.PORT || 3004;
 
 const app = express();
 
+let book = [
+  {
+    _id: 1,
+    name: "harry poter",
+    price: 1299,
+  },
+  {
+    _id: 2,
+    name: "harry poter",
+    price: 1299,
+  },
+  {
+    _id: 3,
+    name: "harry poter",
+    price: 1299,
+  },
+  {
+    _id: 4,
+    name: "harry poter",
+    price: 1299,
+  },
+  {
+    _id: 5,
+    name: "harry poter",
+    price: 1299,
+  },
+  {
+    _id: 6,
+    name: "harry poter",
+    price: 1299,
+  },
+];
+
 app.use(express.json({ limit: "20kb" }));
-app.use(function (req: Request, res: Response, next: NextFunction) {});
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  console.log(`in middleware`);
+  next();
+});
+
+app.get("/getBooks", (req, res) => {
+  return res.json(book);
+});
 
 app.listen(PORT, () => {
   console.log(`server is listnig at PORT ${PORT}`);
