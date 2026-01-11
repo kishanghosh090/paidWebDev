fe06f5338c48be7861eb3f3e99537727f91e3db2
kishanghosh090 <kishanghosh090@gamil.com>
2026-01-11 15:48:22 +0530

    add drizzel

---
+++ NODEJSUDEMY/NODEJS/drizzle/Try.java
@@ -0,0 +1,9 @@
+public class Try {
+    void add(){
+        return ;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ NODEJSUDEMY/NODEJS/drizzle/try.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include <format>
+// // using namespace std;
+// void add()
+// {
+//     // cout << (void)"";
+//     // cout << "hello"
+//     return (void)0;
+// }
+// int main()
+// {
+//     // int i = 12.3;
+//     // int i =(void)0;
+//     // cout << (void)0;
+//     // cout << (string)(int)add();
+//     std::cout << std::format("Hello, {}! You have messages.", add());   
+// }
+
