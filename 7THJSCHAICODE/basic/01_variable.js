2e66d28b8a3dcfe1b370f309851531d7f65a85f8
kishanghosh090 <kishanghosh090@gamil.com>
2026-01-19 14:29:02 +0530

    add data types

---
+++ 7THJSCHAICODE/basic/01_variable.js
@@ -0,0 +1,11 @@
+myVar = "kishan";
+
+// function hello() {}
+// console.log((hello * NaN * hello() + undefined )=== null );
+
+console.log("2" === 2);
+
+let a;
+a = 12;
+a = 23;
+
---
+++ 7THJSCHAICODE/basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+console.log(2 + 3);
+
+// console.log(12 / 0);
+const obj1 = {
+  name: "John",
+};
+const score = undefined;
+// console.log(typeof Number(score));
+
+const myFyun = () => {
+  console.log("hello from");
+};
+
+// console.log(1 + 56 + typeof Number(typeof typeof Function) <= undefined);
+
+// console.log("null" === "null");
+// console.log(Infinity > 0);
+// console.log(Number(""));
+
+console.log("" + myFyun);
