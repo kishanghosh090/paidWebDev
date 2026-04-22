09134a086debf309f8192f78e1985e61f4ac3b09
kishanghosh090 <kishanghosh090@gmail.com>
2026-04-22 21:05:05 +0530

    promise

---
+++ JSYTRIVISON/promise/index.js
@@ -0,0 +1,16 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  // Do an async task, network call
+  fetch("https://randomuser.me/api/11", {
+    method: "GET",
+  })
+    .then((data) => {
+      resolve(data);
+    })
+    .catch((err) => reject(err));
+});
+
+promiseOne
+  .then(async (res) => {
+    console.log(await res.json());
+  })
+  .catch((err) => console.log(err));
